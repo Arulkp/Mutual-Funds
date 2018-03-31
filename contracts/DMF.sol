6fa7fcb94c6c008c836e38db4fa4a9c41c96a48d
Balavignesh <crystelpehonix@gmail.com>
2018-03-31 05:59:04 +0530

    Morning Modify

--- contracts/DMF.sol
+++ contracts/DMF.sol
@@ -5,61 +5,101 @@ import "./FundToken.sol";
 
 contract DMF is FundToken{
     
-        address owner;
-        uint256 rate = 0.001 ether;
+                            //Global Variable Declaration part
+
+        address owner; //owner variable for assigning contract Owner
+        address public newadd; //For getting the Contract Address
+        uint256 rate = 0.001 ether; //rate of Fundtoken For PortfolioManager
+        uint256 cost = 0.01 ether;  //rate of FundToken For Investors
         
-        address public newadd;
+    //ArrayList
+     address[] portfolioMAdd; //Array for storing the each register PortfolioManager
+     address[] Investor; //Array for storing the each register Investors
    
-                       //Phase -2
-            
+    //Phase -2
+                            //Structure Area
+
+
+     //Structure For PortfolioManager Details
             struct PortfolioMFTK
             {
                 address portfolioM;
                 //address contractAdd;
                 uint256 Eth;
             }
-            
+    //Structure For Investor Details  
             struct InvestorPTk
             {
                 address buyer;
                 uint256 Eth;
                 uint256 howTK;
             }
+
+    //Constructor For initialize the contract Owner Address and Contract Deployed Address
             function DMF()public{
                 owner=msg.sender;
                 newadd=address(this);
                 
             }
        
-            
+           
+                             //Mapping Area
+
+      mapping(address => PortfolioMFTK) public BuyTK; //Map for getting and storing the PortfolioManager resgistration Details
       
-      mapping(address => PortfolioMFTK) public BuyTK;
+      //Fallback Function For Holding the Ether in Contract
       function () public payable{
           
       }
       
-        function PortfolioReg(address _portfolioM) public payable{
-            BuyTK[msg.sender].portfolioM=_portfolioM;
+
+                                //Function Area
+        //Function For PortfolioManager Resgistration
+        function PortfolioReg() public payable{
+            BuyTK[msg.sender].portfolioM= msg.sender;
             BuyTK[msg.sender].Eth=BuyTK[msg.sender].Eth + msg.value;
          
             GetFundToken();
         }
+
+        //Function For Getting the Fundtokens by the PortfolioManager
          function GetFundToken() private{
             uint256 tokens = msg.value / rate;
             balanceOf[msg.sender] = balanceOf[msg.sender] + tokens;
             Transfer(0,msg.sender,msg.value);
-            balanceOf[Owner] = balanceOf[Owner] - tokens;
+            portfolioMAdd.push(msg.sender);
          }
 
+        //Function For Getting the Contract Ether Balance 
       function GetBal()public view returns(uint256){
           return this.balance/ 1 ether; //converting wei value to ether
       }
       
+
+      //Function For Getting the Contract Address
       function getContractaddress() public view returns(address)
       {
           return newadd;
       }
       
-    
+      
+      //Phase-3
 
+      //Function For Buying the FundTokens by the Investor From the PortfolioManager
+      function getTokens() public payable
+      {
+          uint256 tokens = msg.value / cost;
+          balanceOf[msg.sender] = balanceOf[msg.sender] + tokens;
+          balanceOf[portfolioMAdd[0]] = balanceOf[portfolioMAdd[0]] - tokens;
+          portfolioMAdd[0].transfer(msg.value);
+          Investor.push(msg.sender);
+      }
+    
+    //Function For Getting the Investor Balance of Tokens
+    function getBalance() public view returns(uint256)
+    {
+        return balanceOf[Investor[0]];
+    }
+
+   
 }
