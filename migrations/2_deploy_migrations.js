2c975a24a8587ae5974d1401900b2c2678cac4d5
Balavignesh <crystelpehonix@gmail.com>
2018-04-01 09:11:29 +0530

    Morning BuleRose

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -1,6 +1,11 @@
-var  FundToken = artifacts.require("FundToken");
-var DMF =  artifacts.require("DMF");
-module.exports = function(deployer) {
-  deployer.deploy(FundToken).then(function(){
-    deployer.deploy(DMF,FundToken.address)});
-};
+var FundToken = artifacts.require('FundToken')
+var DMF = artifacts.require('DMF')
+
+//Problem is resolved The DMF is perfectly deployed ....
+module.exports = function (deployer) {
+  deployer.deploy(FundToken).then(function () {
+    return deployer.deploy(DMF, FundToken.address)
+  })
+}
+
+
